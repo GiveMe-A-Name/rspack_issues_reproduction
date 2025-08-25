414d114aa6be49a2c1b6c4f1ff431221d7c75e45
GiveMe-A-Name <a455300764b@hotmail.com>
2025-08-25 15:54:48 +0800

    first commit

---
+++ recover-error-lazy-compilation/scripts/recover_errors.cjs
@@ -0,0 +1,20 @@
+const fs = require('node:fs')
+const path = require('node:path')
+
+const filePath = path.resolve(__dirname, '../src/App.tsx')
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err)
+    return
+  }
+
+  // Introduce a syntax error
+  const modified = data.replace('<p>Start building amazing things with Rsbuild.</p', '<p>Start building amazing things with Rsbuild.</p>')
+
+  fs.writeFile(filePath, modified, (err) => {
+    if (err) {
+      console.error('Error writing file:', err)
+    }
+  })
+})
